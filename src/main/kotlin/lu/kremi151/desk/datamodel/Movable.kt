e3ff843c0d793b266ee21d503c4c1f30bf34d8be
Michel Kremer <kremi151dev@gmail.com>
2022-09-22 18:56:51 +0200

    Update Movable.kt

--- src/main/kotlin/lu/kremi151/desk/datamodel/Movable.kt
+++ src/main/kotlin/lu/kremi151/desk/datamodel/Movable.kt
@@ -7,11 +7,21 @@ interface Movable {
     val width: Float
     val height: Float
 
+    val locked: Boolean = false
+
     fun remeasure(desiredWidth: Float, desiredHeight: Float)
 
     fun draw(canvas: Canvas)
 
     fun onMoved(x: Float, y: Float) {
+        // No-op by default
+    }
+
+    fun onFocus() {
+        // No-op by default
+    }
+
+    fun onBlur() {
         // No-op by default
     }
 
