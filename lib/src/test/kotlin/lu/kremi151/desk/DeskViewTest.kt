06fc9cb3376bdd0f36dd5d19e0caaa59c69f2cc3
Michel Kremer <kremi151dev@gmail.com>
2022-10-06 18:52:09 +0200

    Add unit test for moving a movable

--- lib/src/test/kotlin/lu/kremi151/desk/DeskViewTest.kt
+++ lib/src/test/kotlin/lu/kremi151/desk/DeskViewTest.kt
@@ -50,10 +50,13 @@ class DeskViewTest {
     }
 
     @Test
-    fun testMovableSelectionAndFocus() {
+    fun testViewSize() {
         assertEquals(800, deskView.width)
         assertEquals(1200, deskView.height)
+    }
 
+    @Test
+    fun testMovableSelectionAndFocus() {
         assertNull(deskView.focusedMovable)
         assertFalse(movable1.focused)
         assertFalse(movable2.focused)
@@ -81,6 +84,42 @@ class DeskViewTest {
         assertNull(deskView.focusedMovable)
         assertFalse(movable1.focused)
         assertFalse(movable2.focused)
+    }
+
+    @Test
+    fun testMovingMovable() {
+        assertEquals(100.0f, movable1.x)
+        assertEquals(100.0f, movable1.y)
+        assertEquals(300.0f, movable2.x)
+        assertEquals(300.0f, movable2.y)
+
+        deskView.onTouchEvent(makeTouch(200f, 200f, MotionEvent.ACTION_DOWN))
+        deskView.onTouchEvent(makeTouch(210f, 205f, MotionEvent.ACTION_MOVE))
+        deskView.onTouchEvent(makeTouch(220f, 210f, MotionEvent.ACTION_MOVE))
+        deskView.onTouchEvent(makeTouch(230f, 215f, MotionEvent.ACTION_MOVE))
+        deskView.onTouchEvent(makeTouch(240f, 220f, MotionEvent.ACTION_MOVE))
+        deskView.onTouchEvent(makeTouch(235f, 210f, MotionEvent.ACTION_MOVE))
+        deskView.onTouchEvent(makeTouch(230f, 200f, MotionEvent.ACTION_MOVE))
+        deskView.onTouchEvent(makeTouch(225f, 190f, MotionEvent.ACTION_MOVE))
+        deskView.onTouchEvent(makeTouch(220f, 180f, MotionEvent.ACTION_MOVE))
+        deskView.onTouchEvent(makeTouch(220f, 180f, MotionEvent.ACTION_UP))
+
+        assertEquals(120.0f, movable1.x)
+        assertEquals(80.0f, movable1.y)
+        assertEquals(300.0f, movable2.x)
+        assertEquals(300.0f, movable2.y)
+
+        deskView.onTouchEvent(makeTouch(350f, 350f, MotionEvent.ACTION_DOWN))
+        deskView.onTouchEvent(makeTouch(340f, 345f, MotionEvent.ACTION_MOVE))
+        deskView.onTouchEvent(makeTouch(330f, 340f, MotionEvent.ACTION_MOVE))
+        deskView.onTouchEvent(makeTouch(320f, 335f, MotionEvent.ACTION_MOVE))
+        deskView.onTouchEvent(makeTouch(310f, 330f, MotionEvent.ACTION_MOVE))
+        deskView.onTouchEvent(makeTouch(310f, 330f, MotionEvent.ACTION_UP))
+
+        assertEquals(120.0f, movable1.x)
+        assertEquals(80.0f, movable1.y)
+        assertEquals(260.0f, movable2.x)
+        assertEquals(280.0f, movable2.y)
     }
 
     private fun makeTouch(x: Float, y: Float, event: Int) = MotionEvent.obtain(
